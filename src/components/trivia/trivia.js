0520358133ad11f20ab363b7f75032e2a07f67ce
Gabriele Romeo <gabriele.romeo@gmail.com>
2017-09-18 15:15:09 +0200

    Add the Trivia component

--- src/components/trivia/trivia.js
+++ src/components/trivia/trivia.js
@@ -1,22 +1,38 @@
 'use strict';
 
-import { Portfolio, lib } from '../../lib';
+import { Portfolio, lib } from '../../js/lib';
 
 const {maybe, maybeValid, activateLoading, deactivateLoading, $http, tail} = lib;
-
 const _$ = Portfolio.Utils.Dom.$;
 
+/**
+ * Starts the loading animation by adding a specific CSS class on the item
+ * @param       {HTMLNode} item The item to be animated
+ * @returns     {Promise} Returns a Promise which, in turns, returns a new
+ *                        HTMLNode having the animation class
+ */
 const startLoadingAnimation = maybe(item => activateLoading(item));
+
+/**
+ * Stops the loading animation by removing a specific CSS class on the item
+ * @param       {HTMLNode} item The animated item
+ * @returns     {Promise} Returns a Promise which, in turns, returns a new
+ *                        HTMLNode without the animation class
+ */
 const stopLoadingAnimation  = maybe(item => deactivateLoading(item));
 
 /**
- * Defines what makes an item valid when a user clicks it
- * @type  {HTMLNode} activeItem  The current active tab item
- * @type  {HTMLNode} clickedItem  The tab label that was clicked by the user
+ * Defines what makes an item to be valid when a user clicks it
+ * @param  {HTMLNode} item  The item that was clicked by a user
  * @returns {Bool} True if the clicked item is valid, False if not
  */
 const predicate = maybe(item =>  item.nodeName === 'I');
 
+/**
+ * Generates a random integer
+ *
+ * @return     {Integer}  A random integer between 0 and 200
+ */
 const randomInteger = () => Math.floor(Math.random() * 200);
 
 const apiReq = function() {
@@ -41,6 +57,9 @@ const callback = {
   }
 };
 
+/**
+ * Retrieves a random fact through an initial API request
+ */
 function init() {
   apiReq()
         .get()
@@ -48,18 +67,32 @@ function init() {
         .catch(callback.error);
 }
 
-const click = maybe(function(target) {
-  let icon = startLoadingAnimation(target);
-
-  apiReq()
-        .get()
-        .then(callback.success)
-        .catch(callback.error)
-        .then(stopLoadingAnimation(icon));
+/**
+ * Handles clicks only when the clicked item satisfies the Trivia's predicate
+ *
+ * @type       {HTMLNode} target The clicked item must be the loading icon
+ */
+const handleClick = maybe(function(target) {
+  startLoadingAnimation(target).then(function(node) {
+    apiReq()
+          .get()
+          .then(callback.success)
+          .catch(callback.error)
+          .then(function() {
+            stopLoadingAnimation(node);
+          });
+  });
 });
 
+/**
+ * Handles click events on the Trivia component.
+ * The clicked item must satisfy the Trivia's predicate otherwise the event
+ * is discarded.
+ *
+ * @param      {Event}  e       A click event
+ */
 function handler(e) {
-  click(maybeValid(predicate)(e.target));
+  handleClick(maybeValid(predicate)(e.target));
 }
 
 export default {
