be97853c4befb3529c5f5565c04b1fdeec2371f5
Gabriele Romeo <gabriele.romeo@gmail.com>
2017-09-22 09:46:55 +0200

    Add components and Clear the code

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,370 +1,215 @@
 'use strict';
 
-
-import { Portfolio } from './lib';
-import tabComponent from './components/tab/tab';
-import cardComponent from './components/card/card';
-import trivia from './components/trivia/trivia';
+import { Portfolio, lib } from './lib';
+import sideMenu from '../components/sideMenu/sideMenu';
+import tabHandler from '../components/tab/tab';
+import cardHandler from '../components/card/card';
+import trivia from '../components/trivia/trivia';
 import documentReady from './document-ready';
 
+const { rcompose } = lib;
+
 function main() {
+  const ANIM = Portfolio.Animation.scrolling;
+  const DOM  = Portfolio.Utils.Dom;
+  const COOKIES = Portfolio.Utils.cookies;
+  const $  = DOM.$;
+  const $$ = DOM.$$;
 
-    const ANIM = Portfolio.Animation.scrolling;
-    const DOM  = Portfolio.Utils.Dom;
-    const COOKIES = Portfolio.Utils.cookies;
-    const $  = DOM.$;
-    const $$ = DOM.$$;
+  const throttleInterval = 500;
+  let isScrolled = true;
+  let lastScrollTop = 0;
 
-    let isScrolled = true;
-    const throttleInterval = 500;
-    let lastScrollTop = 0;
-    const deltaScroll = 5;
+  const DOMcache = {
+    animation: $( '.animation '),
+    header: $( '#header' ),
+    main: $( ' [role="main"] '),
+    backToTop: $( '#backToTop' ),
+    sections:  $$( 'main .c-section' ),
+    sideMenuMobile:  $( '.c-sideMenu' ),
+    sideMenuMobileTrigger: $( '#hamburger__checkbox' ),
+    sideMenuDesktop:  $( '.c-sideMenu--desktop' ),
+    subMenuTrigger: $( '#subMenu--trigger-1' ),
+    projects: $( '#projects' ),
+    graphPaths: $$( '.graph__path--front' ),
+    card: $( '.c-card' ),
+    tab: $( '.c-tab' ),
+    scrollButton: $( '.c-button--scroll' ),
+    randomFactTrigger: $( '.c-random-fact' )
+  };
 
-    let isGraphFirstAnimation = true;
-
-    const DOMcache = {
-        animation: $( '.animation '),
-        header: $( '#header' ),
-        main: $( ' [role="main"] '),
-        backToTop: $( '#backToTop' ),
-        sections:  $$( 'main .c-section' ),
-        sideMenuMobile:  $( '.c-sideMenu' ),
-        sideMenuMobileTrigger: $( '#hamburger__checkbox' ),
-        sideMenuDesktop:  $( '.c-sideMenu--desktop' ),
-        subMenuTrigger: $( '#subMenu--trigger-1' ),
-        projects: $( '#projects' ),
-        graphPaths: $$( '.graph__path--front' ),
-        card: $( '.c-card' ),
-        tab: $( '.c-tab' ),
-        scrollButton: $( '.c-button--scroll' ),
-        randomFactTrigger: $( '.c-random-fact' )
+  function getSystemInformation() {
+    const viewPort = Portfolio.namespace('Utils').Window.viewPort();
+    const body = DOM.getBody();
+    const headerHeight = parseInt(DOM.getComputed(DOMcache.header)('height'));
+    return {
+      isLessThanDesktop: ( viewPort.width < '1024' ) ? true : false,
+      totalScroll: body.scrollTop + headerHeight,
+      VPST: viewPort.height + body.scrollTop,
+      VH: viewPort.height + body.scrollTop - headerHeight,
+      viewPort,
+      body,
+      headerHeight
     };
+  }
 
-    const isLoadingClass = 'is-loading';
+  function handleClick(e) {
+    let target = null;
+    let classL = e.target.classList;
+    let element;
 
-    const welcomeMessages = {
-        visit2: {
-            part1: 'Welcome back',
-            part2: '',
-            part3: 'I feel like you believe in me...',
-            part4: 'isn\'t it<i class="animation__info">?</i>',
-            part5: 'That\'s a wonderful thing!'
-        }
-    };
-
-    let headerHeight = parseInt(DOM.getComputed(DOMcache.header)('height'));
-    let viewPort;
-    let body;
-    let isLessThanDesktop;
-
-
-
-    function getSystemInformation() {
-        body = DOM.getBody();
-        viewPort = Portfolio.namespace('Utils').Window.viewPort();
-        isLessThanDesktop = ( viewPort.width < '1024' ) ? true : false;
+    if ( classL.contains( 'c-navigation__link' ) ||
+         classL.contains( 'c-sideMenu__link' )   ||
+         classL.contains( 'c-scroll' )) {
+      target = e.target;
+    } else if (e.target.parentNode.classList.contains( 'c-scroll' )) {
+      target = e.target.parentNode;
     }
 
-    function handleClick(e) {
-
-        let target = null;
-        let classL = e.target.classList;
-        let element;
-
-
-        if ( classL.contains( 'c-navigation__link' ) ||
-             classL.contains( 'c-sideMenu__link' )   ||
-             classL.contains( 'c-scroll' )) {
-            target = e.target;
-        } else if (e.target.parentNode.classList.contains( 'c-scroll' )) {
-            target = e.target.parentNode;
-        }
-
-        // Make sure an intended element was clicked
-        if ( !target ) {
-            return;
-        }
-
-        e.preventDefault();
-
-        element = $( target.hash );
-
-        ANIM.scrollTo(element, { ease: 'easeOutCubic', duration: 800 });
+    // Make sure that an intended element was clicked
+    if ( !target ) {
+      return;
     }
 
-    function mobileSideMenuClick(e) {
+    element = $(target.hash);
 
-        e.stopPropagation();
+    ANIM.scrollTo(element, { ease: 'easeOutCubic', duration: 800 });
+  }
 
-        let targetSelector;
-        let targetElement;
+  function setSideMenuItem(sysInfo) {
+    sideMenu.setCurrentItemByPosition(sysInfo.totalScroll);
+    return sysInfo;
+  }
 
-        if ( e.target.className === 'c-subMenu__label' ) {
-            targetSelector = e.target.getAttribute( 'for' );
-            targetElement = $( '.c-tab__label[for="' + targetSelector + '"]' );
+  function handleGOUpButton(sysInfo) {
+    let buttonVisibility = DOM.getComputed(DOMcache.backToTop)('visibility');
+    let isVisible = buttonVisibility && ( buttonVisibility === 'visible' );
 
-            // close the subMenu
-            DOMcache.subMenuTrigger.checked = false;
-        } else if ( e.target.nodeName === 'A' ) {
-            e.preventDefault();
-            targetSelector = e.target.getAttribute( 'href' );
-            targetElement = $( targetSelector );
-        } else {
-            return;
-        }
-
-        //When the user clicks on links or the Projects'label, close the menu
-        if (e.target.nodeName === 'A' || e.target.className === 'c-subMenu__label') {
-            DOMcache.sideMenuMobileTrigger.checked = false;
-        }
-
-        // Jump to the element
-        let delay = window.setInterval( function() {
-            Portfolio.Animation.scrolling.scrollTo( targetElement );
-            window.clearInterval(delay);
-        }, 300);
+    // On mobiles do nothing
+    if ( sysInfo.isLessThanDesktop && isVisible ) {
+      DOMcache.backToTop.style.visibility = 'hidden';
+    } else if ( sysInfo.isLessThanDesktop ) {
+      return;
     }
 
-    function showHideScrollUPButton() {
-
-        let buttonVisibility = DOM.getComputed(DOMcache.backToTop)('visibility');
-        let isVisible = buttonVisibility && ( buttonVisibility === 'visible' );
-        let scrolledValue = body.scrollTop + headerHeight;
-
-        // On mobiles do nothing
-        if ( isLessThanDesktop && isVisible ) {
-            DOMcache.backToTop.style.visibility = 'hidden';
-        } else if ( isLessThanDesktop ) {
-            return;
-        }
-
-        activateSideMenuItem(scrolledValue);
-
-        /*
-         * Prevent the system adds or removes the same classes when unnecessary
-         * It does nothing when:
-         *
-         * The scrolled value is bigger than the Viewport's height and
-         * the button is already visible
-         *
-         * The scrolled value is lower than the Viewport's height and
-         * the button is already not visible
-         *
-         */
-        if ( scrolledValue > viewPort.height && isVisible ) {
-            return;
-        } else if ( scrolledValue < viewPort.height && !isVisible ) {
-            return;
-        }
-
-        if ( ( scrolledValue > viewPort.height ) ) {
-            DOMcache.backToTop.style.visibility = 'visible';
-        } else {
-            DOMcache.backToTop.style.visibility = 'hidden';
-        }
-    }
-
-    function showHideHeaderAndSideMenu() {
-
-        let scrolledValue = Math.abs( lastScrollTop - body.scrollTop );
-        const TARGETCLASS = 'c-header--isUp';
-
-        // Make sure the scroll is more than delta
-        if( scrolledValue <= deltaScroll ) {
-            return;
-        }
-
-        // On Desktop it shows the header bar only when the viewport's height is
-        // less than the amount of scroll minus the header's height
-        if ( viewPort.height > viewPort.height + body.scrollTop - headerHeight ) {
-            DOMcache.header.classList.remove(TARGETCLASS);
-            hideSideMenu();
-            return;
-        }
-
-        if ( body.scrollTop > lastScrollTop && body.scrollTop > headerHeight ){
-            // Scroll Down
-            DOMcache.header.classList.add(TARGETCLASS);
-            showSideMenu();
-        } else {
-            // Scroll Up
-
-            // On device less wider tha Desktop it shows the header bar each
-            //time the user srolls up the page
-            if (( viewPort.height <= viewPort.height + body.scrollTop ) &&
-                  isLessThanDesktop ) {
-                DOMcache.header.classList.remove(TARGETCLASS);
-            }
-        }
-
-        // Update lastScrollTop
-        lastScrollTop = body.scrollTop;
-    }
-
-    function showSideMenu() {
-        DOMcache.sideMenuDesktop.style.visibility = 'visible';
-        DOMcache.sideMenuDesktop.style.opacity = 1;
-    }
-
-    function hideSideMenu() {
-        DOMcache.sideMenuDesktop.style.visibility = 'hidden';
-        DOMcache.sideMenuDesktop.style.opacity = 0;
-        // Set the first item
-        setCurrentSideMenuItem('about');
-    }
-
-    function activateSideMenuItem( position ) {
-
-        let coords = Array.from( DOMcache.sections ).map(section => {
-            return {
-                id: section.id,
-                coord: DOM.getElemDistance(section)
-            };
-        }).filter(section => section.coord <= position);
-
-        let id = coords[coords.length -1].id;
-
-        handleGraphAnimation( id );
-        setCurrentSideMenuItem( id );
-    }
-
-    function setCurrentSideMenuItem( id ) {
-
-        let link = $('.c-sideMenu__link[href="#' + id +'"]');
-        let dotNav;
-        let classN;
-        let classL;
-        let currentLink;
-        let currentNav;
-
-        if (!link) {
-            return;
-        }
-
-        dotNav = link.parentNode.previousElementSibling.firstElementChild;
-        classN = dotNav.classList[0] + '--current';
-        classL = link.classList[0]   + '--current';
-
-        currentLink = $( '.' + classL );
-        currentNav  = $( '.' + classN );
-
-        if ( currentLink ) {
-            currentLink.classList.remove(classL);
-        }
-
-        if ( currentNav ) {
-            currentNav.classList.remove(classN);
-        }
-
-        link.classList.add( classL );
-        dotNav.classList.add( classN );
-
-    }
-
-
-    function handleGraphAnimation( id ) {
-        if ( isGraphFirstAnimation && id === 'skills' ) {
-            Array.from( DOMcache.graphPaths ).forEach(path => {
-
-                let circle = DOM.getClosest( path, '.c-circle' );
-
-                path.setAttribute('style', `stroke-dasharray: 629;
-                                            -webkit-animation: load 6s;
-                                            animation: load 6s;`);
-                circle.setAttribute('style', `-webkit-animation: fadeIN 10s;
-                                             animation: fadeIN 10s;`);
-            });
-            isGraphFirstAnimation = false;
-        }
-    }
-
-    function handleScroll() {
-        isScrolled = true;
-    }
-
-    function setCookie() {
-
-        let now = new Date();
-        let oneWeek = 1000 * 60 * 60 * 24 * 7;
-        let counter = COOKIES.get('counter');
-        let count = 0;
-
-        if (!counter || Number.isNaN(parseInt(counter))) {
-            count = 0;
-        } else {
-            count = parseInt(counter) + 1;
-        }
-
-        updateWelcomeMsg(count);
-
-        now.setTime(now.getTime() + oneWeek);
-
-        document.cookie = 'counter=' + count +';expires=' + now.toUTCString();
-    }
-
-    function updateWelcomeMsg(counter) {
-        const animation = DOMcache.animation;
-        const clonedAnimation = DOMcache.animation.cloneNode(true);
-        const messageList = Array.from($$('[id*=welcomeMsg]', clonedAnimation));
-
-        let msgObj;
-
-        if (counter === 0) {
-            return;
-        }
-
-        // Check if a specific welcome msg exists
-        if (welcomeMessages['visit' + counter]) {
-            msgObj = welcomeMessages['visit' + counter];
-        } else {
-            msgObj = welcomeMessages['visit2'];
-        }
-
-        messageList[0].innerHTML = msgObj['part1'];
-        messageList[1].innerHTML = msgObj['part2'];
-        messageList[2].innerHTML = msgObj['part3'];
-        messageList[3].innerHTML = msgObj['part4'];
-        messageList[4].innerHTML = msgObj['part5'];
-
-        animation.parentNode.replaceChild(clonedAnimation, animation);
-    }
-
-    window.setInterval( function() {
-        // Check if the page has been scrolled
-        if ( isScrolled ) {
-            getSystemInformation();
-            showHideScrollUPButton();
-            showHideHeaderAndSideMenu();
-            isScrolled = false;
-        }
-    }, throttleInterval );
-
-
-    // Start custom actions
-    requestAnimationFrame(function() {
-        trivia.init();
-        setCookie();
-    });
-
-
-    /***************************************************************************
+    /*
+     * Prevents the system to add or remove the same classes when unnecessary
+     * It does nothing when:
      *
-     *                         Events listeners
+     * The scrolled value is bigger than the Viewport's height and
+     * the button is already visible
      *
-     ***************************************************************************/
+     * The scrolled value is lower than the Viewport's height and
+     * the button is already not visible
+     *
+     */
+    if ( sysInfo.totalScroll > sysInfo.viewPort.height && isVisible ) {
+        return;
+    } else if ( sysInfo.totalScroll < sysInfo.viewPort.height && !isVisible ) {
+        return;
+    }
 
-    document.addEventListener('scroll', handleScroll, false);
+    if ( ( sysInfo.totalScroll > sysInfo.viewPort.height ) ) {
+      DOMcache.backToTop.style.visibility = 'visible';
+    } else {
+      DOMcache.backToTop.style.visibility = 'hidden';
+    }
+  }
 
-    DOMcache.header.addEventListener('click', handleClick, false);
+  function handleHeaderANDSideMenu(sysInfo) {
+    const ISUP = 'is-up';
+    const ISVISIBLE = 'is-visible';
 
-    DOMcache.scrollButton.addEventListener('click', handleClick, false);
+    // On Desktop it shows the header bar only when the viewport's height is
+    // lesser than the scrolling value minus the header's height
+    if ( sysInfo.viewPort.height > sysInfo.VH ) {
+      DOMcache.header.classList.remove(ISUP);
+      DOMcache.sideMenuDesktop.classList.remove(ISVISIBLE);
+      //return;
+    }
 
-    DOMcache.sideMenuDesktop.addEventListener('click', handleClick, false);
-    DOMcache.sideMenuMobile.addEventListener('click', mobileSideMenuClick, false);
+    if ( sysInfo.body.scrollTop > lastScrollTop       &&
+         sysInfo.body.scrollTop > sysInfo.headerHeight ) {
+        // Scroll Down
+      DOMcache.header.classList.add(ISUP);
+      DOMcache.sideMenuDesktop.classList.add(ISVISIBLE);
+    } else {
+      // Scroll Up
+      // On device less wider than Desktop it shows the header bar each
+      // time the user srolls up the page
+      if ( sysInfo.isLessThanDesktop === true &&
+           sysInfo.viewPort.height <= sysInfo.VPST ) {
+        DOMcache.header.classList.remove(ISUP);
+      }
+    }
 
-    DOMcache.card.addEventListener('click', cardComponent, false);
-    DOMcache.tab.addEventListener('click', tabComponent, false);
-    DOMcache.randomFactTrigger.addEventListener('click', trivia.handler, false);
+    // Update lastScrollTop
+    lastScrollTop = sysInfo.body.scrollTop;
+    return sysInfo;
+  }
+
+  function handleScroll() {
+    isScrolled = true;
+  }
+
+  function setCookie() {
+    const now = new Date();
+    const oneWeek = 1000 * 60 * 60 * 24 * 7;
+    const counter = COOKIES.get('counter');
+    let count = 0;
+
+    if (!counter || Number.isNaN(parseInt(counter))) {
+      count = 0;
+    } else {
+      count = parseInt(counter) + 1;
+    }
+
+    now.setTime(now.getTime() + oneWeek);
+
+    document.cookie = 'counter=' + count +';expires=' + now.toUTCString();
+  }
+
+  const runSystem = rcompose(
+    handleHeaderANDSideMenu,
+    setSideMenuItem,
+    handleGOUpButton
+  );
+
+  window.setInterval( function() {
+    // Check if the page has been scrolled
+    if ( isScrolled ) {
+      const sysInfo = getSystemInformation();
+      runSystem(sysInfo);
+      isScrolled = false;
+    }
+  }, throttleInterval );
+
+
+  // Start custom actions
+  requestAnimationFrame(function() {
+    trivia.init();
+    setCookie();
+  });
+
+
+  /***************************************************************************
+   *
+   *                         Events listeners
+   *
+   ***************************************************************************/
+
+  document.addEventListener('scroll', handleScroll, false);
+
+  DOMcache.header.addEventListener('click', handleClick, false);
+  DOMcache.scrollButton.addEventListener('click', handleClick, false);
+  DOMcache.sideMenuDesktop.addEventListener('click', handleClick, false);
+
+  DOMcache.sideMenuMobile.addEventListener('click', sideMenu.mobileClickHandler, false);
+
+  DOMcache.card.addEventListener('click', cardHandler, false);
+  DOMcache.tab.addEventListener('click', tabHandler, false);
+
+  DOMcache.randomFactTrigger.addEventListener('click', trivia.handler, false);
 }
 
 // When DOM is ready, call the main function
