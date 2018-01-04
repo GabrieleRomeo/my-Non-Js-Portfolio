b54caa458e9b6fb1b9227b9db87d271ddd846f39
Gabriele Romeo <gabriele.romeo@gmail.com>
2018-01-04 19:47:17 +0100

    Add the getConfig function

--- gulp.config.js
+++ gulp.config.js
@@ -1,2 +1,12 @@
+const fs = require('fs');
+
 export const RELEASEBRANCH = 'release-branch';
 export const VERSIONING = '?v=@version@';
+
+/**
+ * Utility function that returns the package.json config file
+ */
+export const getConfig = () => {
+  const json = JSON.parse(fs.readFileSync('./package.json'));
+  return json;
+};
