b865c520e32df09d0feda0534a245d9a4da8c858
Gabriele Romeo <gabriele.romeo@gmail.com>
2018-01-05 14:43:40 +0100

    Remove the _images subtask and fix reference path

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,24 +1,22 @@
 module.exports = function(gulp, plugins, PATHS, browserSync, onError) {
   return function() {
-    gulp.task('_images', false, () => {
-      return gulp
-        .src(['**/*.{png,gif,jpg}'], { cwd: `../${PATHS.IMAGES_SRC}` })
-        .pipe(
-          plugins.cache(
-            plugins.imagemin({
-              optimizationLevel: 5,
-              progressive: true,
-              interlaced: true,
-            }),
-          ),
-        )
-        .pipe(
-          plugins.print(filepath => {
-            return 'Image built: ' + filepath;
+    gulp
+      .src(['**/*.{png,gif,jpg}'], { cwd: `${PATHS.IMAGES_SRC}` })
+      .pipe(
+        plugins.cache(
+          plugins.imagemin({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true,
           }),
-        )
-        .on('error', onError)
-        .pipe(gulp.dest(PATHS.IMAGES_DST));
-    });
+        ),
+      )
+      .pipe(
+        plugins.print(filepath => {
+          return 'Image built: ' + filepath;
+        }),
+      )
+      .on('error', onError)
+      .pipe(gulp.dest(PATHS.IMAGES_DST));
   };
 };
