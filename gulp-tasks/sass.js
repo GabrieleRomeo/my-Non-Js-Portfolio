6b35bea7aecc4dce56c203a0b12204520a4bc7f9
Gabriele Romeo <gabriele.romeo@gmail.com>
2018-01-05 14:31:19 +0100

    Save source maps into a separate file

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -3,16 +3,18 @@ module.exports = function(gulp, plugins, PATHS, browserSync, onError) {
   const { development, production } = plugins.environments;
   return function() {
     gulp
-      .src(`../${PATHS.CSS_SRC}/main.scss`)
+      .src(`${PATHS.CSS_SRC}/main.scss`)
+      .pipe(plugins.debug({ title: 'debug-src' }))
       .pipe(plugins.sass({ importer: nodeSassGlobbing }))
       .pipe(plugins.autoprefixer('last 2 version'))
       .pipe(plugins.rename({ suffix: '.min' }))
       .pipe(development(plugins.sourcemaps.init()))
       .pipe(plugins.cssnano())
-      .pipe(development(plugins.sourcemaps.write()))
+      .pipe(development(plugins.sourcemaps.write('maps')))
       .pipe(production(plugins.size({ showFiles: true })))
       .on('error', onError)
-      .pipe(production(gulp.dest(`../${PATHS.CSS_DST}`)))
+      .pipe(gulp.dest(`${PATHS.CSS_DST}`))
+      .pipe(plugins.debug({ title: 'destination' }))
       .pipe(browserSync.stream());
   };
 };
